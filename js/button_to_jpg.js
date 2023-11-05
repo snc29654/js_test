12af210727c510e216ab809ad047ec3b4c61049a
snc29654 <snc29654l@nifty.com>
2023-11-06 08:06:09 +0900

    button_to_jpg追加

---
+++ js/button_to_jpg.js
@@ -0,0 +1,6 @@
+function buttonClick1(){
+    document.write("<img  src=\"img/img.jpg\" width=\"200\" height=\"150\">");
+}
+function buttonClick2(){
+    document.write("<img  src=\"img/img2.jpg\" width=\"200\" height=\"150\">");
+}
