6bce9addd6ec4ab72cd38b8e0726bb6d46790801
snc29654 <snc29654@nifty.com>
2023-11-06 09:28:56 +0900

    戻るボタン追加

--- js/button_to_jpg.js
+++ js/button_to_jpg.js
@@ -1,6 +1,14 @@
 function buttonClick1(){
     document.write("<img  src=\"img/img.jpg\" width=\"200\" height=\"150\">");
+    document.write("<input type=\"button\" value=\"戻る\" onclick=\"buttonClick3()\">");
+
+
 }
 function buttonClick2(){
     document.write("<img  src=\"img/img2.jpg\" width=\"200\" height=\"150\">");
+    document.write("<input type=\"button\" value=\"戻る\" onclick=\"buttonClick3()\">");
+}
+
+function buttonClick3(){
+    window.location.reload();
 }
