9c190979affeb1c00d1910cd5c480c2107a740e8
snc29654 <snc29654@nifty.com>
2023-11-07 09:26:04 +0900

    複数画像表示ボタン追加

--- js/button_to_jpg.js
+++ js/button_to_jpg.js
@@ -10,6 +10,20 @@ function buttonClick2(){
     document.write("<p>花壇</p>");
 }
 
+
+function buttonClick5(){
+    document.write("<img  src=\"../../jpg/1.jpg\" width=\"200\" height=\"150\">");
+    document.write("<input type=\"button\" value=\"戻る\" onclick=\"buttonClick3()\">");
+    document.write("<p>無題</p>");
+    document.write("<img  src=\"../../jpg/20.jpg\" width=\"200\" height=\"150\">");
+    document.write("<input type=\"button\" value=\"戻る\" onclick=\"buttonClick3()\">");
+    document.write("<p>無題</p>");
+    document.write("<img  src=\"../../jpg/30.jpg\" width=\"200\" height=\"150\">");
+    document.write("<input type=\"button\" value=\"戻る\" onclick=\"buttonClick3()\">");
+    document.write("<p>無題</p>");
+}
+
+
 function buttonClick3(){
     window.location.reload();
 }
